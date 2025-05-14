0e940acaa9006d42a975a9e3be71a350a839dc49
Choikyudong <slgmgm@naver.com>
2025-05-14 15:31:04 +0900

    Fix: 게시글 목록 쿼리 수정
    - 게시글 조회가 제대로 표현안되는 버그 수정
    - 조회순 목록 쿼리 수정

--- src/main/java/kyulab/postservice/dto/res/PostListItemDto.java
+++ src/main/java/kyulab/postservice/dto/res/PostListItemDto.java
@@ -8,18 +8,18 @@ import java.time.LocalDateTime;
  * @param userId		작성자 아이디
  * @param subject		제목
  * @param summary		요약본
+ * @param createdAt		작성일자
  * @param viewCount		조회수
  * @param commentCount	댓글 수
- * @param createdAt		작성일자
  */
-public record PostListItemDto(long id, long userId, String subject, String summary, long viewCount, long commentCount, LocalDateTime createdAt) {
-	public PostListItemDto(long id, long userId, String subject, String summary, long viewCount, long commentCount, LocalDateTime createdAt) {
+public record PostListItemDto(long id, long userId, String subject, String summary, LocalDateTime createdAt, long viewCount, long commentCount) {
+	public PostListItemDto(long id, long userId, String subject, String summary, LocalDateTime createdAt, long viewCount, long commentCount) {
 		this.id = id;
 		this.userId = userId;
 		this.subject = subject;
 		this.summary = summary;
+		this.createdAt = createdAt;
 		this.viewCount = viewCount;
 		this.commentCount = commentCount;
-		this.createdAt = createdAt;
 	}
 }
